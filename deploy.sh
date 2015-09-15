b19908494180813741ad09ead6f607afb99b8e7e
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 16:39:20 +0200

    lkjgfkgd

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ VERSION="$MAJOR.$MINOR.$PATCH"
 
 echo $VERSION > test
 
+# Set up a bit of configuration
 git config --local user.name $GH_USER
 git config --local user.username $GH_USER
 git remote add deploy https://$GH_USER:${GH_TOKEN}@github.com/$GH_USER/$GH_REPO.git
+
+# Do the release commit
 git checkout -b "release-$VERSION"
 git add test
 git commit -m "Patate"
-git push deploy release-$VERSION <ignore >ignore 2>ignore
-rm -rf ignore
+
+# Silent push to avoid the token to be shown in the console ^.^
+touch ___ignore
+git push deploy release-$VERSION <___ignore >___ignore 2>___ignore
+rm -rf ___ignore

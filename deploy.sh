20f47bdc7ed012bd75e36a230d52504263f31028
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 16:35:52 +0200

    [patch] test

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,15 @@ GH_REPO=$(basename `git rev-parse --show-toplevel`)
 PATCH=$(git log --oneline | grep -c "\[ *patch *\]")
 MINOR=$(git log --oneline | grep -c "\[ *minor *\]")
 MAJOR=$(git log --oneline | grep -c "\[ *major *\]")
+VERSION="$MAJOR.$MINOR.$PATCH"
 
-echo "$MAJOR.$MINOR.$PATCH"
+echo $VERSION > test
 
-touch test
-
+git config --local user.name $GH_USER
+git config --local user.username $GH_USER
 git remote add deploy https://$GH_USER:${GH_TOKEN}@github.com/$GH_USER/$GH_REPO.git
-git checkout -b "release-x.x.x"
+git checkout -b "release-$VERSION"
 git add test
 git commit -m "Patate"
-git push deploy release-x.x.x > ignore
+git push deploy release-$VERSION <ignore >ignore 2>ignore
+rm -rf ignore

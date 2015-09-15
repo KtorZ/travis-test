8f59d87671b2b8e0bf80c596bc2440b6ef452423
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 15:54:39 +0200

    [minor ] dflgkjdflkgd

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 touch test
 git config --local user.name "Travis"
+git config --local user.username "TravisTheSmiths"
 git config --local user.email "matthias.benkort+travis@gmail.com"
+git config --local -l
 git checkout -b "release-x.x.x"
 git add test
 git commit -m "Patate"

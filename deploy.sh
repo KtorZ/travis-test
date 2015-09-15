9d2ea601fc9c04c9eed90157a8a24400c36a739a
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 15:50:20 +0200

    [major] test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 touch test
+git config --local user.name "Travis"
+git config --local user.email "matthias.benkort+travis@gmail.com"
 git checkout -b "release-x.x.x"
 git add test
 git commit -m "Patate"

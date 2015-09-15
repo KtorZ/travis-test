298abc7a894f884b6992648071c70d5bc4f0a855
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 16:09:57 +0200

    [patch] gkdfjg

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+GH_REPO=$(basename `git rev-parse --show-toplevel`)
+
 touch test
-git config --local user.name "Travis"
-git config --local user.username "TravisTheSmiths"
-git config --local user.email "matthias.benkort+travis@gmail.com"
-git config --local -l
+
+git remote add deploy https://$GH_USER:${GH_TOKEN}@github.com/$GH_USER/$GH_REPO.git
 git checkout -b "release-x.x.x"
 git add test
 git commit -m "Patate"
-git push origin release-x.x.x
+git push deploy release-x.x.x

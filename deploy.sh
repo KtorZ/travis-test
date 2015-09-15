43ee8c52e047bbb752019a257e37aef360ad8773
Matthias Benkort <matthias.benkort@gmail.com>
2015-09-15 16:21:27 +0200

    [minor] kdfgjdf

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,16 @@
 
 GH_REPO=$(basename `git rev-parse --show-toplevel`)
 
+PATCH=$(git log --oneline | grep -c "\[ *patch *\]")
+MINOR=$(git log --oneline | grep -c "\[ *minor *\]")
+MAJOR=$(git log --oneline | grep -c "\[ *major *\]")
+
+echo "$MAJOR.$MINOR.$PATCH"
+
 touch test
 
 git remote add deploy https://$GH_USER:${GH_TOKEN}@github.com/$GH_USER/$GH_REPO.git
 git checkout -b "release-x.x.x"
 git add test
 git commit -m "Patate"
-git push deploy release-x.x.x
+git push deploy release-x.x.x > ignore
